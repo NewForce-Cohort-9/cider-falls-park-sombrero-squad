e0f567de99d85836acc2095ca14a33b600f73479
Fiona Wee <fwee@oswego.edu>
2024-05-03 12:43:49 -0500

    commit before PR

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { ServiceList } from "./services.js";
 import { Navbar } from "../UI/navBar.js";
 import { guestList } from "./guests.js";
 
+
 const mainContainer = document.querySelector("#container");
 const services = document.querySelector(".services");
 const navContainer = document.querySelector(".navbar");
@@ -11,5 +12,6 @@ const guests = document.querySelector(".guests");
 services.innerHTML = ServiceList();
 navContainer.innerHTML = Navbar();
 guests.innerHTML = guestList();
+
 
 
