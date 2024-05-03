bfcbe38375da89162abd4cf54e9e4ee4c42db5eb
Iris Lee <irislee4git@gmail.com>
2024-05-03 11:16:25 -0400

    display dynamic html for destinations and css

--- scripts/destinations.js
+++ scripts/destinations.js
@@ -9,33 +9,36 @@ const services = getServices();
 
 const findServices = (destination) => {
     let serviceIdList = [];
-    let services = null;
+    let servicesOffered = '<ul>';
 
-    for(const serviceDestination of serviceDestinations) {
+    for (const serviceDestination of serviceDestinations) {
         if  (serviceDestination.destinationId === destination.id) {
                 serviceIdList.push(serviceDestination.serviceId)
         }
-    return serviceIdList;
     }
 
-    for (const serviceId of serviceIdList) {
-        if (serviceId === service.id ) {
-            services += service.name
+    for (let i = 0; i < serviceIdList.length; i++) {
+        for (const service of services) {
+            if (serviceIdList[i] === service.id ) {
+                servicesOffered += `<li>${service.name}</li>`
+            }
+            
         }
-
-
     }
-    return services;
+    servicesOffered += '</ul>'
+    return servicesOffered;
 }
 
-// export const Destinations = () => {
-//     let html = ''
+export const Destinations = () => {
+    let html = '<div class="card">'
 
-//     for (const destination of destinations) {
-//         html += `<div class="card_detail"><img src="${destination.picture}"/>
-//                         <ul>
-//                         </ul>
-//                         </div>`
-//     }
-// 
+    for (const destination of destinations) {
+        const servicesOffered = findServices(destination)
 
+        html += `<div class="card_detail"><h3>${destination.name}</h3><img src="${destination.picture}"/>
+                        <div class="card_services">Services Offered: ${servicesOffered}</div>
+                        </div>`
+    }
+    html += '</div>'
+    return html;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
+import { Destinations } from "./destinations.js";
+
+// for services
 const mainContainer = document.querySelector("#container");
-
-import { findServices } from "./destinations.js";
-
-findServices();
+mainContainer.innerHTML = Destinations();
